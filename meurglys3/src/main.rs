35ad678852e8f67f77049821fe7595318e5c33d6
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2024-08-27 20:00:12 +0200

    i guess i was a little retarded so i fixed how the args get parsed

--- meurglys3/src/main.rs
+++ meurglys3/src/main.rs
@@ -6,47 +6,57 @@ use meurglys3_lib::{self, Package};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    dir: Option<PathBuf>,
-    #[arg(short, long)]
-    source: Option<OsString>,
-    #[arg(short, long)]
-    unpack: Option<bool>,
-    #[arg(short, long)]
-    output: Option<OsString>,
-    #[arg(short, long, value_parser, num_args = 1.., value_delimiter = ',')]
-    check: Option<Vec<String>>,
+    #[command(subcommand)]
+    target: Target,
+    // #[arg(short, long)]
+    // dir: Option<PathBuf>,
+    // #[arg(short, long)]
+    // source: Option<OsString>,
+    // #[arg(short, long)]
+    // unpack: Option<bool>,
+    // #[arg(short, long)]
+    // output: Option<OsString>,
+    // #[arg(short, long, value_parser, num_args = 1.., value_delimiter = ',')]
+    // check: Option<Vec<String>>,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Target {
+    Pack {
+        dir: PathBuf,
+        out: OsString
+    },
+    Unpack{
+        dir: OsString,
+        out: OsString
+    },
+    Check {
+        dir: OsString,
+        #[clap(value_parser, num_args = 1.., value_delimiter = ',')]
+        check: Vec<String>
+    }
 }
 
 fn main() {
     let mut pack: Option<Package> = None;
     let args = Args::parse();
-    if let Some(true) = args.unpack {
-        let Some(file) = args.source.clone() else {
-            panic!("source file not specified");
-        };
-        pack = Some(unpack(
-            file,
-            args.output.clone().expect("no unpack output specified"),
-        ));
-    }
-    if let Some(check) = args.check {
-        return match (args.source, pack) {
-            (Some(source), None) => {
-                pack = Some(meurglys3_lib::load_package(source).unwrap());
-                check_pack(&check, &pack.unwrap())
-            }
-            (None, Some(pack)) => check_pack(&check, &pack),
-            _ => {
-                panic!("cannot perform a check without the package path specified")
-            }
-        };
-    }
-    if let Some(dir) = args.dir {
-        let mut pack = meurglys3_lib::package_dir(dir).expect("Failed to package");
-        let out = args.output.unwrap_or("package".into());
-        meurglys3_lib::write_package(out, &mut pack).expect("failed to write package");
-    }
+
+    match args.target {
+        Target::Pack { dir, out } => {
+            let mut pack = meurglys3_lib::package_dir(dir).expect("Failed to package");
+            meurglys3_lib::write_package(out, &mut pack).expect("failed to write package");
+        },
+        Target::Unpack { dir, out } => {
+            pack = Some(unpack(
+                dir,
+                out,
+            ));
+        },
+        Target::Check { dir, check } => {
+            pack = Some(meurglys3_lib::load_package(dir.clone()).expect(&format!("could not load package at `{}`", dir.to_str().unwrap_or_default())));
+            check_pack(&check, &pack.unwrap())
+        }
+    };
 }
 fn unpack(source: OsString, dest: OsString) -> Package {
     let pack = meurglys3_lib::load_package(source).expect("unpack failed");
