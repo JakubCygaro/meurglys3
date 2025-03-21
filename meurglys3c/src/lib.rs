73b6ea9bbc99055e42297734671812d135bafde3
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2025-03-21 01:33:08 +0100

    meu3_free_package

--- meurglys3c/src/lib.rs
+++ meurglys3c/src/lib.rs
@@ -2,6 +2,12 @@ use std::ffi::{c_char, c_void, CString};
 
 use meurglys3_lib;
 
+#[repr(C)]
+pub enum Meu3Error {
+    NoError = -1,
+    StringError,
+    PackError
+}
 //#[repr(C)]
 //pub struct MeurglysPackage {
 //    pub names: *mut c_void,//HashMap<String, DataInfo>,
@@ -12,11 +18,25 @@ use meurglys3_lib;
 
 #[no_mangle]
 extern "C" {
-    pub unsafe fn package_dir(dir_path: *const c_char) -> *mut c_void {
+    pub unsafe fn meu3_package_dir(dir_path: *const c_char) -> *mut c_void {
+        if dir_path.is_null() {
+            return std::ptr::null_mut::<c_void>()
+        }
         let path = CString::from_raw(dir_path).to_str().expect("failed to read dir_path string");
         let pack = meurglys3_lib::package_dir(path);
         let pack = pack.expect("failed to package directory");
         let pack = Box::new(pack);
         Box::into_raw(pack) as *mut c_void
+    }
+    pub fn meu3_free_package(package: *mut c_void) {
+        if !package.is_null() {
+            let pack = unsafe {
+                Box::from_raw(package as *mut meurglys3_lib::Package)
+            };
+            drop(pack);
+        }
+    }
+    pub fn meu3_load_package(dir_path: *const c_char) -> *mut c_void {
+
     }
 }
