184355edd5078c9aeab8f101b61142fd589c37e3
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2025-03-30 04:19:39 +0200

    kinda getting there

--- meurglys3c/tests/pack.c
+++ meurglys3c/tests/pack.c
@@ -1,5 +1,11 @@
 #include "meu3.h"
 
 int main(void) {
+    MEU3_Error err = -1;
+    MEU3_PACKAGE* pack = meu3_package_dir("test_dir", &err);
+    if(!pack) {
+        return 1;
+    }
+    meu3_free_package(pack);
     return 0;
 }
